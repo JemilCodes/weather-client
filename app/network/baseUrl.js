babb40b7e3ffc3d30d0a55850bc145500ccb7782
JemilCodes <jemiluishaqodoba@gmail.com>
2024-01-21 16:19:13 +0100

    deploy

--- app/network/baseUrl.js
+++ app/network/baseUrl.js
@@ -2,6 +2,6 @@ const enviroment = process.env.NODE_ENV;
 const baseUrl =
   enviroment === "development"
     ? "http://localhost:5000/weather/api/v1"
-    : "https://precious-bonbon-a624d7.netlify.app/api/auth";
+    : "replaceme/weather/api/v1";
 
 export default baseUrl;
